cc12cf04f89c883f142261888735a98924d3f2d1
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-03 11:46:06 -0300

    Creating gulp task for compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
+
+// compile sass into css and auto-inject into browsers
+
+gulp.task('sass', function()
+{
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+
+});
