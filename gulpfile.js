26aba30dc5c1003f1f3c2923e95459cf7bed67b3
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-03 11:50:13 -0300

    gulp tasck for move the javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,21 @@ var sass = require('gulp-sass');
 
 // compile sass into css and auto-inject into browsers
 
-gulp.task('sass', function()
-{
+gulp.task('sass', function () {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-    .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+
+});
+
+
+//move the javascript files into our /src/js folder
+
+gulp.task('js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
 
 });
