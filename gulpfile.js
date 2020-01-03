a32f86f16f18620542a5356fd28d15a951428192
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-03 11:56:25 -0300

    server + watching scss/html files

--- gulpfile.js
+++ gulpfile.js
@@ -22,3 +22,13 @@ gulp.task('js', function () {
         .pipe(browserSync.stream());
 
 });
+
+// static server + watching scss/html files
+
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        server: "./src"
+    });
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
